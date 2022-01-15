c314bfd4f27e7e5246a5bd0e6d332df81a100153
Rahul Raj K <51423724+RAHULRAJ77K@users.noreply.github.com>
2022-01-16 02:03:47 +0530

    Add files via upload

---
+++ He/Task1/excited_states/By/he_y_3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Parallel job submission script:
+# Usage: qsub <this_script>
+# The shell used to run the job
+#$ -S /bin/bash
+#
+# The name of the parallel queue to submit the job to
+#
+# Define the parallel runtime environment and number of nodes
+# NB: number of nodes is one more than needed as on e copy resides on the master node
+#$ -pe mpi 1
+# Use location that job was submitted as working directory
+#$ -cwd
+#
+# Export all environment variables to the slave jobs
+#$ -V
+#
+# Put stdout & stderr into the same file
+#$ -j y
+#
+# The name of the SGE logfile for this job
+#$ -o output.log
+#mpiexec -n $ NSLOTS ./heat
+/home/sangita.sen/packages/london/london.x file_y3.inp >file_y3.out
