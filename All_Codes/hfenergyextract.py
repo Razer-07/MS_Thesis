1bfeb8c6ff1eb383d9581b33d175d2364e9ddfc1
Rahul Raj K <51423724+Razer-07@users.noreply.github.com>
2023-03-20 18:43:43 +0530

    Update hfenergyextract.py

--- All_Codes/hfenergyextract.py
+++ All_Codes/hfenergyextract.py
@@ -1,7 +1,8 @@
+import numpy as np
 L=[]
 for i in range(11):
     with open('file_y'+str(i)+'.out') as line:
         l=line.readlines()
     L.append(float(l[591][33:51]))    
-import numpy as np
+
 np.savetxt('xvalue.txt',np.array(L))    
